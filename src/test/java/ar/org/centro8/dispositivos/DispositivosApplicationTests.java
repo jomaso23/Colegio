08a2955d4f4454e9e9c34aa62a3466d069d2752a
jomaso <jomaso23@gmail.com>
2024-05-15 20:14:07 -0300

    modificando el archivo de testeo agregando la ruta a @SpringBootTest(classes = CursosApplication.class)

--- src/test/java/ar/org/centro8/dispositivos/DispositivosApplicationTests.java
+++ src/test/java/ar/org/centro8/dispositivos/DispositivosApplicationTests.java
@@ -1,9 +1,11 @@
 package ar.org.centro8.dispositivos;
 
+import ar.org.centro8.colegio.CursosApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = CursosApplication.class)
+
 class DispositivosApplicationTests {
 
 	@Test
